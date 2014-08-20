aa39e33ee1db8d4501a035cfde2e6814bb08fbec
Eugene Aleynikov <a1j@users.noreply.github.com>
2014-08-19 19:26:26 -0700

    cleanup

--- PIC24F Blinker.X/blinker.h
+++ PIC24F Blinker.X/blinker.h
@@ -2,7 +2,7 @@
  * File:   blinker.h
  * Author: Eugene Aleynikov
  *
- * Created on August 12, 2014, 10:21 PMa1j@users.noreply.github.com
+ * Created on August 12, 2014, 10:21 PM a1j@users.noreply.github.com
  */
 
 #ifndef BLINKER_H
@@ -12,9 +12,6 @@
 extern "C" {
 #endif
 
-void __attribute__((__interrupt__,__no_auto_psv__)) _AddressError(void);
-void __attribute__((__interrupt__,__no_auto_psv__)) _StackError(void);
-void __attribute__((__interrupt__,__auto_psv__)) _T1Interrupt(void);
 void initMCU(void);
 void initLed(void);
 void resetTimer(void);
